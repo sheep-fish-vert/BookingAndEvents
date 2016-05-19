4149b6309ebd50b0648a9c83e6a93fc63d1b9dcb
Art G <artem.garmatyk@gmail.com>
2016-05-19 16:15:06 +0300

    add

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -1,8 +1,56 @@
+ /* plugin */ 
 
-
-
+ /* plugin */
 $(document).ready(function(){
+    
 
+  console.log($('.main').find('#artist-conteiner').length);
+ 
+  console.log($('.main').find('#artist-list').length);
+  
+  if( $('.main').find('#artist-conteiner').length == 1 ){
+      
+      var wall = new Freewall("#artist-conteiner");
+      wall.reset({
+        selector: '.box',
+        animate: true,
+        cellW: 256,
+        cellH: 256,
+        draggable: true,
+        onResize: function() {
+            wall.fitWidth();
+        },
+        onBlockMove: function() {
+            wall.fitWidth();
+        }
+    });
+    wall.fitWidth();
+      
+  }
+  
+  if( $('.main').find('#artist-list').length == 1 ){
+      console.log('tyt');
+      var wall = new Freewall("#artist-list");
+      wall.reset({
+        selector: '.box',
+        animate: true,
+        cellW: 560,
+        cellH: 100,
+        fixSize: 0,
+        gutterX: 20,
+        gutterY: 10,
+        draggable: true,
+        onResize: function() {
+            wall.fitWidth();
+        },
+        onBlockMove: function() {
+            wall.fitWidth();
+        }
+    });
+    wall.fitWidth();
+      
+  }
+  
 });
 
 $(window).load(function(){
