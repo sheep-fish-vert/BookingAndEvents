a43f863b0ace9aca843b1f9cfdadeb4c4ea9dec8
Nazar <kryvyynazariy@gmail.com>
2016-05-24 16:35:55 +0300

    pop up

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -2,7 +2,22 @@
 
 
 $(document).ready(function(){
-
+	$(".fancybox-form").fancybox({
+		padding: [20, 50, 40, 50],
+		width:1000,
+		height: 600,
+		autoSize: true,
+		autoResize: true,
+		fitTiVie: true,
+		autoCenter: true,
+		helpers:{
+			overlay: {
+				css:{
+					'background': 'rgba(255, 255, 255, 0)' 
+				}
+			}
+		}	
+	});
 });
 
 $(window).load(function(){
